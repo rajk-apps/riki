8322f0b1dc2fe64f185c0b4559609a71159b57cf
Endre Márk Borza <endremborza@gmail.com>
2020-07-08 23:35:28 +0200

    update nb command

--- dev_env/settings_add.py
+++ dev_env/settings_add.py
@@ -1,8 +1,13 @@
 import os
 from .settings import *
 
-INSTALLED_APPS = [os.environ.get("APP_NAME")] + INSTALLED_APPS
+INSTALLED_APPS = [
+    os.environ.get("APP_NAME"),
+    "django_extensions",
+] + INSTALLED_APPS
 
 ROOT_URLCONF = "{}.url_add".format(os.environ.get("DJANGO_PROJECT"))
 
 ALLOWED_HOSTS += [os.environ.get("DJANGO_HOST"), "localhost"]
+
+NOTEBOOK_ARGUMENTS = ["--ip", "0.0.0.0", "--port", "8888", "--allow-root"]
